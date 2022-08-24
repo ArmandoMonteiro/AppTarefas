9b9b4c0b9a0872f446a0bf5e5cbff86105505316
ArmandoMonteiro <armandomonteirojr@outlook.com>
2022-08-24 15:16:02 -0300

    first deply

---
+++ AppTarefas/Classes/Tarefa.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace AppTarefas.Classes
+{
+    public class Tarefa
+    {
+        public int IdTarefa { get; set; }
+        public string DescTarefa { get; set; }
+        public int IdStatus { get; set; }
+        public string DescStatus { get; set; }
+
+        public void CriarTarefa(Tarefa objTarefa)
+        {
+            
+
+            BancoDeDadosApi objConexaoDB = new BancoDeDadosApi();
+
+            string Sql = "Insert Into TbTarefas (DescTarefa,IdStatus) values ('" + objTarefa.DescTarefa + "',1)";
+
+            objConexaoDB.ExecutaComando(Sql);
+            objConexaoDB.Dispose();
+        }
+
+        public List<Tarefa> ListarTarefas()
+        {
+            List<Tarefa> ListaTarefas = new List<Tarefa>();
+
+            BancoDeDadosApi objConexaoDB = new BancoDeDadosApi();
+
+            string Sql = "Select IdTarefa,DescTarefa,DescStatus "
+                + "From TbTarefas, TbStatus "
+                + "Where TbTarefas.IdStatus = TbStatus.IdStatus";
+
+            SqlDataReader reader = objConexaoDB.ExecutaComRetorno(Sql);
+
+            while (reader.Read())
+            {
+                Tarefa objTarefa = new Tarefa();
+
+                objTarefa.IdTarefa = int.Parse(reader["IdTarefa"].ToString());
+                objTarefa.DescTarefa = reader["DescTarefa"].ToString();
+                objTarefa.DescStatus = reader["DescStatus"].ToString();
+
+                ListaTarefas.Add(objTarefa);
+            }
+
+            objConexaoDB.Dispose();
+            return ListaTarefas;
+        }
+
+        public Tarefa BuscarTarefa(int IdTarefa)
+        {
+            
+            BancoDeDadosApi objConexaoDB = new BancoDeDadosApi();
+
+            string Sql = "Select IdTarefa,DescTarefa "
+                + "From TbTarefas "
+                + "Where TbTarefas.Idtarefa = " + IdTarefa;
+
+            SqlDataReader reader = objConexaoDB.ExecutaComRetorno(Sql);
+
+            Tarefa objTarefa = new Tarefa();
+            while (reader.Read())
+            {
+                objTarefa.IdTarefa = int.Parse(reader["IdTarefa"].ToString());
+                objTarefa.DescTarefa = reader["DescTarefa"].ToString();
+            }
+
+            objConexaoDB.Dispose();
+            return objTarefa;
+        }
+
+        public void ApagarTarefa(int IdTarefa)
+        {
+            BancoDeDadosApi objConexaoDB = new BancoDeDadosApi();
+
+            string Sql = "Delete From TbTarefas Where IdTarefa = " + IdTarefa;
+
+            objConexaoDB.ExecutaComando(Sql);
+            objConexaoDB.Dispose();
+        }
+
+        public void AlterarTarefa(int IdTarefa, int IdStatus)
+        {
+            BancoDeDadosApi objConexaoDB = new BancoDeDadosApi();
+
+            string Sql = "Update TbTarefas Set IdStatus = " + IdStatus + " Where IdTarefa = " + IdTarefa;
+
+            objConexaoDB.ExecutaComando(Sql);
+            objConexaoDB.Dispose();
+        }
+    }
+}
---
+++ AppTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,74 @@
+﻿using AppTarefas.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppTarefas.Controllers
+{
+    public class TarefaController : Controller
+    {
+
+        
+        public ActionResult ListarTarefas()
+        {
+            Tarefa objTarefa = new Tarefa();
+
+            return View(objTarefa.ListarTarefas());
+        }
+
+        
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Create(Tarefa objTarefa)
+        {
+            try
+            {
+                objTarefa.CriarTarefa(objTarefa);
+
+                return RedirectToAction("ListarTarefas");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        
+        public ActionResult Delete(int id)
+        {
+            Tarefa objTarefa = new Tarefa();
+            objTarefa.ApagarTarefa(id);
+            return RedirectToAction("ListarTarefas");
+        }
+
+        public ActionResult Update(int Id)
+        {
+            Tarefa objTarefa = new Tarefa();
+            objTarefa = objTarefa.BuscarTarefa(Id);
+
+            Status ObjStatus = new Status();
+            List<Status> dadosStatus = ObjStatus.ListarStatus();
+            SelectList lista = new SelectList(dadosStatus, "IdStatus", "DescStatus");
+            ViewBag.ListaStatus = lista;
+
+            return View(objTarefa);
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, int IdStatus)
+        {
+            Tarefa objTarefa = new Tarefa();
+            objTarefa.AlterarTarefa(id,IdStatus);
+            return RedirectToAction("ListarTarefas");
+        }
+    }
+}
